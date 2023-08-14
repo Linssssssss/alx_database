c4a5f8c4352b0412d837d790d805b88117d7deb6
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-14 11:10:42 +0300

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,14 +1,8 @@
 -- This a script that prints the full description of the table first_table from the database hbtn_0c_0
-#!/bin/bash
-
-# Get the database name from the command line arguments
-database=$1
-
-# Connect to the database
-mysql -hlocalhost -uroot -p $database
-
-# Get the CREATE TABLE statement for the table `first_table`
-table_create=`SHOW CREATE TABLE first_table`;
-
-# Print the CREATE TABLE statement
-echo $table_create
+CREATE TABLE first_table (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(128) DEFAULT NULL,
+  c char(1) DEFAULT NULL,
+  created_at date DEFAULT NULL,
+  PRIMARY KEY (id)
+);
