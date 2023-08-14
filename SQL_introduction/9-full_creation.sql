2429725acf59d98d8926c7e542c5f2d8f437cea2
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-14 10:42:19 +0300

    Full creation

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- This is a script that creates a table in the database in the MySQL server and adds multiple rows
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+INSERT INTO second_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(1, 'George', 8);
