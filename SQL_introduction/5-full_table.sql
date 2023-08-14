5af5ca8163319d6c3396eb4490ffbe11f5fc8a35
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-14 11:07:20 +0300

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,16 @@
 -- This a script that prints the full description of the table first_table from the database hbtn_0c_0
 mysql -hlocalhost -uroot -p hbtn_0c_0 <<EOF
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
+SELECT CONCAT('CREATE TABLE `', TABLE_NAME, '` (',
+  GROUP_CONCAT(
+    CONCAT('`', COLUMN_NAME, '`', ' ', COLUMN_TYPE,
+           IF(IS_NULLABLE = 'NO', ' NOT NULL', ''),
+           IF(COLUMN_DEFAULT IS NOT NULL, CONCAT(' DEFAULT ', QUOTE(COLUMN_DEFAULT)), ''),
+           IF(COLUMN_KEY = 'PRI', ' PRIMARY KEY', ''),
+           IF(EXTRA = 'auto_increment', ' AUTO_INCREMENT', '')
+          )
+    SEPARATOR ',\n'
+  ),
+  ') ENGINE=', ENGINE, ' DEFAULT CHARSET=', CHARACTER_SET_NAME, ';')
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
 EOF
