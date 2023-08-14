7fa95241b7f00bafb430e64a2b3ed441a0110b22
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-14 11:13:55 +0300

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,8 +1,2 @@
--- This a script that prints the full description of the table first_table from the database hbtn_0c_0
-CREATE TABLE first_table (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  name varchar(128) DEFAULT NULL,
-  c char(1) DEFAULT NULL,
-  created_at date DEFAULT NULL,
-  PRIMARY KEY (id)
-);
+-- Prints the full description of the table first_table.
+SHOW CREATE TABLE `first_table`;
