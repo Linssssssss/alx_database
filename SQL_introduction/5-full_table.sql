895ec38b602fcfcf7c85a19006e0406c8b6944e2
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-14 11:09:12 +0300

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,16 +1,14 @@
 -- This a script that prints the full description of the table first_table from the database hbtn_0c_0
-mysql -hlocalhost -uroot -p hbtn_0c_0 <<EOF
-SELECT CONCAT('CREATE TABLE `', TABLE_NAME, '` (',
-  GROUP_CONCAT(
-    CONCAT('`', COLUMN_NAME, '`', ' ', COLUMN_TYPE,
-           IF(IS_NULLABLE = 'NO', ' NOT NULL', ''),
-           IF(COLUMN_DEFAULT IS NOT NULL, CONCAT(' DEFAULT ', QUOTE(COLUMN_DEFAULT)), ''),
-           IF(COLUMN_KEY = 'PRI', ' PRIMARY KEY', ''),
-           IF(EXTRA = 'auto_increment', ' AUTO_INCREMENT', '')
-          )
-    SEPARATOR ',\n'
-  ),
-  ') ENGINE=', ENGINE, ' DEFAULT CHARSET=', CHARACTER_SET_NAME, ';')
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
-EOF
+#!/bin/bash
+
+# Get the database name from the command line arguments
+database=$1
+
+# Connect to the database
+mysql -hlocalhost -uroot -p $database
+
+# Get the CREATE TABLE statement for the table `first_table`
+table_create=`SHOW CREATE TABLE first_table`;
+
+# Print the CREATE TABLE statement
+echo $table_create
