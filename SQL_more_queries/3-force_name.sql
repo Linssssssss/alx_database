a8c3ae64d434644035bfbcbd7653c9b3143e0b80
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-20 13:21:50 +0300

    Always a name

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,9 +1,5 @@
--- create_table_force_name.sql
-
-USE hbtn_test_db_3; -- Use the correct database name here
-
--- Create table if not exists
+-- Create the table if it doesn't exist
 CREATE TABLE IF NOT EXISTS force_name (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT,
     name VARCHAR(256) NOT NULL
 );
