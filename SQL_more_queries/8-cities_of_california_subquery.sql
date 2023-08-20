f918b7476f26a3b662f0d8f7031946cefbea1696
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-20 13:15:27 +0300

    8 cities of carlifornia

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,3 @@
--- cities_of_california_subquery.sql
-
-USE hbtn_0d_usa;
-
--- List cities of California
-SELECT * FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'California')
-ORDER BY id;
+-- Lists all the cities of California
+SELECT id, name FROM cities
+WHERE state_id IN (SELECT id FROM states WHERE name = 'California');
