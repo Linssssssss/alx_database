ad372c3dce0e09c413b1ac2de00916a241dd63c1
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-20 13:17:15 +0300

    Unique ID

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,10 +1,3 @@
--- create_table_unique_id.sql
-
-USE your_database_name; -- Replace with the actual database name
-
--- Create table if not exists
-CREATE TABLE IF NOT EXISTS unique_id (
-    id INT NOT NULL DEFAULT 1,
-    name VARCHAR(256),
-    UNIQUE KEY id (id)
-);
+-- Creates the table id_not_null on MySQL server.
+CREATE TABLE IF NOT EXISTS unique_id(
+id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
