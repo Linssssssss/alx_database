6676d54c32b51294f58d3ed99f3719847ef289c2
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-08-20 12:52:21 +0300

    Read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,10 @@
--- Creates the database hbtn_0d_2 and the user user_0d_2.
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
-GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost' IDENTIFIED BY
-'user_0d_2_pwd';
+-- create_db_and_user.sql
+
+-- Create database if not exists
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create user if not exists
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege to user_0d_2 on hbtn_0d_2 database
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
